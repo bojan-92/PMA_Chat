e22c63009290d5c3316fb524cb61023c57742e19
bojan12345 <bojantrifkovic92@gmail.com>
2017-04-17 17:16:53 +0200

    Create splash screen class

---
+++ app/src/main/java/com/pma/chat/pmaChat/splash/SplashScreen.java
@@ -0,0 +1,56 @@
+package com.pma.chat.pmaChat.splash;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.pma.chat.pmaChat.MainActivity;
+import com.pma.chat.pmaChat.R;
+
+/**
+ * Created by Bojan on 4/17/2017.
+ */
+
+public class SplashScreen extends Activity {
+
+    ProgressBar progressBar;
+    int progressStatus = 0;
+    TextView textView;
+    Handler handler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        textView = (TextView)findViewById(R.id.load_per);
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+
+        new Thread(new Runnable() {
+            public void run() {
+                while (progressStatus < 100) {
+                    progressStatus += 1;
+                    handler.post(new Runnable() {
+                        public void run() {
+                            progressBar.setProgress(progressStatus);
+                            textView.setText(progressStatus + "%");
+                        }
+                    });
+                    try {
+                        Thread.sleep(30);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                if (progressStatus == 100) {
+                    Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                    startActivity(i);
+                }
+            }
+        }).start();
+    }
+}
