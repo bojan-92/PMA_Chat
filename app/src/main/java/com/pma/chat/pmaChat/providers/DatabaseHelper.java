83d25ba2634b89a4a7b3aee0832931df2134357a
bojan12345 <bojantrifkovic92@gmail.com>
2017-06-15 00:35:38 +0200

    Contacts

---
+++ app/src/main/java/com/pma/chat/pmaChat/providers/DatabaseHelper.java
@@ -0,0 +1,106 @@
+package com.pma.chat.pmaChat.providers;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.RuntimeExceptionDao;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+import com.pma.chat.pmaChat.model.Contact;
+
+/**
+ * Created by Bojan on 6/14/2017.
+ */
+
+public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
+
+    // name of the database file for your application -- change to something appropriate for your app
+    private static final String DATABASE_NAME = "helloAndroid.db";
+    // any time you make changes to your database objects, you may have to increase the database version
+    private static final int DATABASE_VERSION = 1;
+
+    // the DAO object we use to access the SimpleData table
+    private Dao<Contact, Integer> simpleDao = null;
+    private RuntimeExceptionDao<Contact, Integer> simpleRuntimeDao = null;
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db, ConnectionSource connectionSource) {
+        try {
+            Log.i(DatabaseHelper.class.getName(), "onCreate");
+            TableUtils.createTable(connectionSource, Contact.class);
+        } catch (SQLException e) {
+            Log.e(DatabaseHelper.class.getName(), "Can't create database", e);
+            throw new RuntimeException(e);
+        } catch (java.sql.SQLException e) {
+            e.printStackTrace();
+        }
+
+        // here we try inserting data in the on-create as a test
+        RuntimeExceptionDao<Contact, Integer> dao = getContactDao();
+
+        Contact contact = new Contact(1000L, "Novi Kontakt");
+        dao.create(contact);
+
+        Log.i(DatabaseHelper.class.getName(), "created new entries in onCreate: " + contact.getName());
+    }
+
+    /**
+     * This is called when your application is upgraded and it has a higher version number. This allows you to adjust
+     * the various data to match the new version number.
+     */
+    @Override
+    public void onUpgrade(SQLiteDatabase db, ConnectionSource connectionSource, int oldVersion, int newVersion) {
+        try {
+            Log.i(DatabaseHelper.class.getName(), "onUpgrade");
+            TableUtils.dropTable(connectionSource, Contact.class, true);
+            // after we drop the old databases, we create the new ones
+            onCreate(db, connectionSource);
+        } catch (SQLException e) {
+            Log.e(DatabaseHelper.class.getName(), "Can't drop databases", e);
+            throw new RuntimeException(e);
+        } catch (java.sql.SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns the Database Access Object (DAO) for our SimpleData class. It will create it or just give the cached
+     * value.
+     */
+    public Dao<Contact, Integer> getDao() throws SQLException, java.sql.SQLException {
+        if (simpleDao == null) {
+            simpleDao = getDao(Contact.class);
+        }
+        return simpleDao;
+    }
+
+    /**
+     * Returns the RuntimeExceptionDao (Database Access Object) version of a Dao for our SimpleData class. It will
+     * create it or just give the cached value. RuntimeExceptionDao only through RuntimeExceptions.
+     */
+    public RuntimeExceptionDao<Contact, Integer> getContactDao() {
+        if (simpleRuntimeDao == null) {
+            simpleRuntimeDao = getRuntimeExceptionDao(Contact.class);
+        }
+        return simpleRuntimeDao;
+    }
+
+    /**
+     * Close the database connections and clear any cached DAOs.
+     */
+    @Override
+    public void close() {
+        super.close();
+        simpleDao = null;
+        simpleRuntimeDao = null;
+    }
+}
