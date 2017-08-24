1a018b7c782af8ac5039cc5eb9ece3a074f90dc1
davidmilivojev <davyd7_@hotmail.com>
2017-08-24 03:23:39 +0200

    add profile edit and display

--- app/src/main/java/com/pma/chat/pmaChat/fragments/ProfileSettingsFragment.java
+++ app/src/main/java/com/pma/chat/pmaChat/fragments/ProfileSettingsFragment.java
@@ -4,16 +4,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.pma.chat.pmaChat.R;
 import com.pma.chat.pmaChat.auth.LoginActivity;
+import com.pma.chat.pmaChat.auth.SignupActivity;
+import com.pma.chat.pmaChat.model.User;
+import com.pma.chat.pmaChat.utils.RemoteConfig;
 
 
 public class ProfileSettingsFragment extends Fragment {
@@ -23,7 +35,13 @@ public class ProfileSettingsFragment extends Fragment {
     private EditText txtLastName;
     private Button btnSave;
 
-    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth mFirebaseAuth;
+
+    private DatabaseReference mRootDatabaseReference = FirebaseDatabase.getInstance().getReference();
+
+    private User mUser;
+
+    private ChildEventListener mChildEventListener;
 
     @Nullable
     @Override
@@ -31,18 +49,43 @@ public class ProfileSettingsFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_profile_settings, container, false);
 
-        firebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        String userId = mFirebaseAuth.getCurrentUser().getUid();
+        final DatabaseReference currentUserRef = mRootDatabaseReference.child(RemoteConfig.USER).child(userId);
 
         tvLogout = (TextView) view.findViewById(R.id.tvLogout);
         btnSave = (Button) view.findViewById(R.id.btnSaveProfileSettings);
 
-        txtFirstName = (EditText) view.findViewById(R.id.txtFirstName);
-        txtLastName = (EditText) view.findViewById(R.id.txtLastName);
+        txtFirstName = (EditText) view.findViewById(R.id.txtProfileSettingsFirstName);
+        txtLastName = (EditText) view.findViewById(R.id.txtProfileSettingsLastName);
+//        String firstName = txtFirstName.getText().toString().trim();
+//        String lastName = txtLastName.getText().toString().trim();
+
+        currentUserRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                mUser = dataSnapshot.getValue(User.class);
+
+                txtFirstName.setText(mUser.getFirstName());
+                txtLastName.setText(mUser.getLastName());
+
+
+
+//                String firstName = .getText().toString().trim();
+//                 String lastName = txtLastName.getText().toString().trim();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         tvLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                firebaseAuth.signOut();
+                mFirebaseAuth.signOut();
                 Intent i = new Intent(getActivity().getApplicationContext(), LoginActivity.class);
                 startActivity(i);
             }
@@ -51,12 +94,44 @@ public class ProfileSettingsFragment extends Fragment {
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 String firstName = txtFirstName.getText().toString().trim();
                 String lastName = txtLastName.getText().toString().trim();
+
+                if(!isFormValid(firstName, lastName) || mUser == null) {
+                    return;
+                }
+
+                mUser.setFirstName(firstName);
+                mUser.setLastName(lastName);
+
+                currentUserRef.setValue(mUser, new DatabaseReference.CompletionListener() {
+                    @Override
+                    public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                        // TODO : add some notification (toast or something)
+                        Log.i("TODO-MESSAGE", "TODO-MESSSAGE");
+                    }
+                });
+
+
             }
         });
 
         return view;
+    }
+
+
+    private boolean isFormValid(String firstName, String lastName) {
+        if (TextUtils.isEmpty(firstName)) {
+
+            return false;
+        }
+        if (TextUtils.isEmpty(lastName)) {
+
+            return false;
+        }
+
+        return true;
     }
 }
 
